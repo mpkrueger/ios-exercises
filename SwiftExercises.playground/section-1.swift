81f6027092fc854c2db654f4b8bb6adce76872bf
mpkrueger <mpkrueger@gmail.com>
2015-06-13 12:48:53 -0700

    completed all exercises in the swift playground

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is \(cheese)"
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -20,13 +20,16 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+    numberArray.append(5)
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
+numberDictionary
 
 /*
 
@@ -35,10 +38,16 @@ Loops
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
-// WORK HERE
+
+for i in 1...10 {
+    println(i)
+}
+
 
 // Use a half-closed range loop to print 1 - 10, inclusively
-// WORK HERE
+for i in 1..<11 {
+    println(i)
+}
 
 let worf = [
     "name": "Worf",
@@ -53,12 +62,22 @@ let picard = [
     "information": "Captain of the USS Enterprise",
     "favorite drink": "tea, Earl Grey, hot"]
 
+
+
 let characters = [worf, picard]
+picard["favorite drink"]
+
+
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    
+    // Here I initiated the favDrinks array with a string - but how would I initiate an empty array? I kept getting a type error
+    var favDrinks = ["tea"]
+    for char in characters {
+        favDrinks.append(char["favorite drink"]!)
+    }
+    return favDrinks
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -76,6 +95,19 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func stringsArrayIntoSemicolonString(strings:Array<String>) -> String {
+    var expectedOutput = ""
+    for item in strings {
+        if item == strings[0] {
+            expectedOutput = item
+        } else {
+            expectedOutput = expectedOutput + ";" + item
+        }
+    }
+    return expectedOutput
+}
+
+stringsArrayIntoSemicolonString(strings)
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -89,3 +121,6 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+var cerealsSorted = sorted(cerealArray, {cereal1, cereal2 in cereal1 < cereal2})
+var cerealsSorted1 = sorted(cerealArray, <)
